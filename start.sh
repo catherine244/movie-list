21d74724d67116f8f6f0e2f4a46c82dbc38c39bf
catherine244 <ccayreen24@gmail.com>
2019-12-28 17:02:47 +0300

    initial commit

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='5ecabdbe419eeda80f605c702c18c68f'
+export SECRET_KEY='Yousecretkey'
+export MAIL_USERNAME='blablabla@gmail.com'
+export MAIL_PASSWORD='blabla'
+python manage.py server
+
